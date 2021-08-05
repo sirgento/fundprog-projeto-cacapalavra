5e7f994a0264d8ff1e684007c31b28f506a2e4d7
nashinoki <nashinoki>
2021-08-05 09:42:03 -0300

    Adicionando arquivos

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "./resource/headers/typedefs.h"
+
+int main(int argc, char **argv) {
+	setlocale(LC_ALL, " ");
+
+}
---
+++ resource/headers/typedefs.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+typedef char TinyString[16];
+typedef char ShortString[32];
+typedef char String[64];
+typedef char LongString[128];
+typedef char HugeString[256];
+typedef char ShortText[512];
+typedef char Text[1024];
+typedef char LongText[2048];
+typedef char HugeText[4096];
